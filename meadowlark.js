c3352998554fd8fd1da67119b5b0ad0b4665dd7b
Manu Uzkudun <manuuzkudun@hotmail.com>
2015-11-10 20:38:52 +0100

    Handlebars view engine and some basic views added

--- meadowlark.js
+++ meadowlark.js
@@ -3,19 +3,31 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
-// custom 404 page
-app.use(function(req, res){
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+  .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+  res.render('home');
 });
 
-// custom 500 page
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+  res.status(404);
+  res.render('404');
+});
+
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
